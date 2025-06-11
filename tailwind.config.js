d3c54b26d777829e25bee14a5f87a13b5967b746
David Miranda <david@storylog.com>
2025-06-10 22:54:56 -0400

    tailwind build

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './renderer.html',
+    './new-app-ui.html',
+    './preload.js',
+    './main.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
