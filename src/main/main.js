bc97f0fcb6ae27baf530dddc82d5d3e1c27138cd
David Miranda <david@storylog.com>
2025-12-04 09:21:03 -0500

    chore: release v1.2.2

--- src/main/main.js
+++ src/main/main.js
@@ -19,8 +19,8 @@ if (process.platform === 'darwin') {
   const iconPath = path.join(__dirname, '../../assets/icons/icon.png');
   const aboutOptions = {
     applicationName: 'Hyperclay Local',
-    applicationVersion: '1.2.1',
-    version: '1.2.1',
+    applicationVersion: '1.2.2',
+    version: '1.2.2',
     copyright: 'Made with ❤️ for Hyperclay'
   };
 
